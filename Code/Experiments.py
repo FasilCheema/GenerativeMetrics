4c4981928f2422ba475b1d01eaeda359c3a07ecc
FasilCheema <faisal.cheema4@gmail.com>
2022-01-09 21:38:25 -0500

    updated experiments list

--- Code/Experiments.py
+++ Code/Experiments.py
@@ -10,7 +10,7 @@ def ExperimentQueue():
     Main function to run all experiments
     '''
     #All experiments for reproducibility are intialized with the same random seed of 7
-    fig_num = 1377
+    fig_num = 1384
 
 
     #All experiments for reproducibility are intialized with the same random seed of 7
@@ -18,7 +18,7 @@ def ExperimentQueue():
     #C is a constant for setting an appropriate value of k' = Ck for the PR cover algorithm
     print(fig_num)
     r_seed = 7 
-    k = 3
+    k = 39
     C = 3
     n = 3000
     m = 3000
